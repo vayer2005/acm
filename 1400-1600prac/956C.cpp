d9cee8f05b63cd7a6a201c3e14a999781301f5a7
Vittal Ayer <vayer2005@gmail.com>
2025-05-17 22:12:40 -0700

    1025

---
+++ 1400-1600prac/956C.cpp
@@ -0,0 +1,387 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define int long long
+#define pb push_back
+#define ppb pop_back
+#define mp make_pair
+using namespace std;
+using vpi = vector<pair<int, int>>;
+using pi = pair<int, int>;
+using vi = vector<int>;
+using vvi = vector<vector<int>>;
+#define ff first
+#define ss second
+#define all(x) x.begin(), x.end()
+#define sz(x) (int)(x).size()
+const int inf = 9e18;
+const int mod = 1e9 + 7;
+const int NUM = 1000030;
+const int N = 2e5+5;
+#define DEBUG(x) cerr << #x << ": " << x << '\n'
+template <typename T, typename Y>
+istream &operator>>(istream &is, pair<T, Y> &p)
+{
+    is >> p.first >> p.second;
+    return is;
+}
+template <typename T, typename Y>
+ostream &operator<<(ostream &os, pair<T, Y> p)
+{
+    os << p.first << ' ' << p.second << ' ';
+    return os;
+}
+template <typename T>
+istream &operator>>(istream &is, vector<T> &v)
+{
+    for (auto &i : v)
+        is >> i;
+    return is;
+}
+template <typename T>
+ostream &operator<<(ostream &os, vector<T> v)
+{
+    for (auto &i : v)
+        os << i << ' ';
+    return os;
+}
+
+
+vector<int> lp, sieve;
+vector<int> pr;
+vector<int> power;
+vector<int> fact(2e5 + 5, 0);
+void initpow(int x)
+{
+    power.resize(NUM);
+    power[0] = 1;
+    for (int i = 1; i < NUM; i++)
+    {
+        power[i] = (power[i - 1] * (x % mod)) % mod;
+    }
+}
+void initFactorial()
+{
+    fact.resize(NUM);
+    fact[0] = 1;
+    for (int i = 1; i < NUM; i++)
+    {
+        fact[i] = (fact[i - 1] * i) % mod;
+    }
+}
+void calc_sieve()
+{
+    sieve.resize(NUM + 1, 0);
+    for (int x = 2; x <= NUM; x++)
+    {
+        if (sieve[x])
+            continue;
+        for (int u = x; u <= NUM; u += x)
+        {
+            sieve[u] = x;
+        }
+    }
+}
+void primefactor()
+{
+    lp.resize(N + 1, 0);
+    for (int i = 2; i <= N; ++i)
+    {
+        if (lp[i] == 0)
+        {
+            lp[i] = i;
+            pr.push_back(i);
+        }
+        for (int j = 0; j < (int)pr.size() && pr[j] <= lp[i] && i * pr[j] <= N; ++j)
+            lp[i * pr[j]] = pr[j];
+    }
+}
+int binpow(int a, int b)
+{
+    int res = 1;
+    while (b > 0)
+    {
+        if (b & 1)
+            res = res * a;
+        a = a * a;
+        b >>= 1;
+    }
+    return res;
+}
+int binpow(int a, int b, int mod)
+{
+    int res = 1;
+    while (b > 0)
+    {
+        if (b & 1)
+            res = (res * a) % mod;
+        a = (a * a) % mod;
+        b >>= 1;
+    }
+    return res % mod;
+}
+int gcd(int a, int b)
+{
+    if (b == 0)
+        return a;
+    else
+        return gcd(b, a % b);
+}
+int lcm(int a, int b)
+{
+    return ((a / gcd(a, b)) * b);
+}
+int inversemod(int a, int mod)
+{
+    return binpow(a, mod - 2, mod);
+}
+int divmod(int a, int b, int c)
+{
+    return ((a % c) * inversemod(b, c)) % c;
+}
+int C(int n, int k)
+{
+    if (k > n)
+        return 0;
+    int p1 = (fact[n] * inversemod(fact[k], mod)) % mod;
+    int p2 = (1 * inversemod(fact[n - k], mod)) % mod;
+    return (p1 * p2) % mod;
+}
+ 
+void solve()
+{
+    int n;
+    cin >> n;
+    vi a(n,0);
+    vi b(n,0);
+    vi c(n,0);
+    cin >> a >> b >> c;
+    double tsum = 0;
+
+    for(int i = 0; i < n; i++) {
+        tsum += a[i];
+    }
+    int val = ceil(tsum/3.0);
+
+    //ABC
+    int aSum = 0;
+    int bSum = 0;
+    int cSum = 0;
+    int al, ar, bl, br, cl, cr = -1;
+
+    for(int i = 0; i < n; i++) {
+        if (aSum >= val) {
+            if (bSum >= val) {
+                if (cSum == 0) {
+                    br = i;
+                    cl = i+1; 
+                }
+                cSum += c[i];
+            } else {
+                if (bSum == 0) {
+                    ar = i;
+                    bl = i+1; 
+                }
+                bSum += b[i];
+            }
+        } else {
+            if (aSum == 0) {
+                al = i+1;
+            }
+            aSum += a[i];
+        }
+    }
+
+    cr = n;
+    if (aSum >= val && bSum >= val && cSum >= val) {
+        cout << al << " " << ar << " " << bl << " " << br << " " << cl << " " << cr << endl;
+        return;
+    }
+
+    //ACB
+    aSum = 0;
+    bSum = 0;
+    cSum = 0;
+
+    for(int i = 0; i < n; i++) {
+        if (aSum >= val) {
+            if (cSum >= val) {
+                if (bSum == 0) {
+                    cr = i;
+                    bl = i+1; 
+                }
+                bSum += b[i];
+            } else {
+                if (cSum == 0) {
+                    ar = i;
+                    cl = i+1; 
+                }
+                cSum += c[i];
+            }
+        } else {
+            if (aSum == 0) {
+                al = i+1;
+            }
+            aSum += a[i];
+        }
+    }
+    br = n;
+
+    if (aSum >= val && bSum >= val && cSum >= val) {
+        cout << al << " " << ar << " " << bl << " " << br << " " << cl << " " << cr << endl;
+        return;
+    }
+
+
+    //BAC
+    aSum = 0;
+    bSum = 0;
+    cSum = 0;
+    for(int i = 0; i < n; i++) {
+        if (bSum >= val) {
+            if (aSum >= val) {
+                if (cSum == 0) {
+                    ar = i;
+                    cl = i+1; 
+                }
+                cSum += c[i];
+            } else {
+                if (aSum == 0) {
+                    br = i;
+                    al = i+1; 
+                }
+                aSum += a[i];
+            }
+        } else {
+            if (bSum == 0) {
+                bl = i+1;
+            }
+            bSum += b[i];
+        }
+    }
+
+    cr = n;
+    if (aSum >= val && bSum >= val && cSum >= val) {
+        cout << al << " " << ar << " " << bl << " " << br << " " << cl << " " << cr << endl;
+        return;
+    }
+
+    //BCA
+    aSum = 0;
+    bSum = 0;
+    cSum = 0;
+    for(int i = 0; i < n; i++) {
+        if (bSum >= val) {
+            if (cSum >= val) {
+                if (aSum == 0) {
+                    cr = i;
+                    al = i+1; 
+                }
+                aSum += a[i];
+            } else {
+                if (cSum == 0) {
+                    br = i;
+                    cl = i+1; 
+                }
+                cSum += c[i];
+            }
+        } else {
+            if (bSum == 0) {
+                bl = i+1;
+            }
+            bSum += b[i];
+        }
+    }
+
+    ar = n;
+
+    if (aSum >= val && bSum >= val && cSum >= val) {
+        cout << al << " " << ar << " " << bl << " " << br << " " << cl << " " << cr << endl;
+        return;
+    }
+
+
+    //CAB
+    aSum = 0;
+    bSum = 0;
+    cSum = 0;
+    for(int i = 0; i < n; i++) {
+        if (cSum >= val) {
+            if (aSum >= val) {
+                if (bSum == 0) {
+                    ar = i;
+                    bl = i+1; 
+                }
+                bSum += b[i];
+            } else {
+                if (aSum == 0) {
+                    cr = i;
+                    al = i+1; 
+                }
+                aSum += a[i];
+            }
+        } else {
+            if (cSum == 0) {
+                cl = i+1;
+            }
+            cSum += c[i];
+        }
+    }
+
+    br = n;
+
+    if (aSum >= val && bSum >= val && cSum >= val) {
+        cout << al << " " << ar << " " << bl << " " << br << " " << cl << " " << cr << endl;
+        return;
+    }
+
+    //CBA
+    aSum = 0;
+    bSum = 0;
+    cSum = 0;
+    for(int i = 0; i < n; i++) {
+        if (cSum >= val) {
+            if (bSum >= val) {
+                if (aSum == 0) {
+                    br = i;
+                    al = i+1; 
+                }
+                aSum += a[i];
+            } else {
+                if (bSum == 0) {
+                    cr = i;
+                    bl = i+1; 
+                }
+                bSum += b[i];
+            }
+        } else {
+            if (cSum == 0) {
+                cl = i+1;
+            }
+            cSum += c[i];
+        }
+    }
+
+    ar = n;
+    if (aSum >= val && bSum >= val && cSum >= val) {
+        cout << al << " " << ar << " " << bl << " " << br << " " << cl << " " << cr << endl;
+        return;
+    }
+
+    cout << -1 << endl;
+
+
+
+}
+
+signed main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    int t;
+    cin >> t;
+    fact[0] = 1;
+	for(int64_t i = 1; i < N; ++i) fact[i] = (fact[i - 1] * i) % mod;
+    while (t--)
+        solve();
+    return 0;
+}
