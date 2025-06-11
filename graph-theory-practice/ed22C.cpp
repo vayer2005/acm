41bb32d310d5fcf741fcb0e3b045e26aece3ec6e
Vittal Ayer <vayer2005@gmail.com>
2025-06-10 21:30:03 -0700

    graph problems

---
+++ graph-theory-practice/ed22C.cpp
@@ -0,0 +1,208 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
+using namespace std;
+
+#define int long long
+#define pb push_back
+#define ppb pop_back
+#define mp make_pair
+using vpi = vector<pair<int, int>>;
+using pi = pair<int, int>;
+using vi = vector<int>;
+using vvi = vector<vector<int>>;
+#define ff first
+#define ss second
+#define all(x) x.begin(), x.end()
+#define sz(x) (int)(x).size()
+const int inf = 9e18;
+const int mod = 1e9 + 7;
+const int NUM = 1000030;
+const int N = 2e5+5;
+
+typedef __gnu_pbds::tree<int, __gnu_pbds::null_type, less<int>, __gnu_pbds::rb_tree_tag, __gnu_pbds::tree_order_statistics_node_update> ordered_set;
+
+#define DEBUG(x) cerr << #x << ": " << x << '\n'
+template <typename T, typename Y>
+istream &operator>>(istream &is, pair<T, Y> &p)
+{
+    is >> p.first >> p.second;
+    return is;
+}
+template <typename T, typename Y>
+ostream &operator<<(ostream &os, pair<T, Y> p)
+{
+    os << p.first << ' ' << p.second << ' ';
+    return os;
+}
+template <typename T>
+istream &operator>>(istream &is, vector<T> &v)
+{
+    for (auto &i : v)
+        is >> i;
+    return is;
+}
+template <typename T>
+ostream &operator<<(ostream &os, vector<T> v)
+{
+    for (auto &i : v)
+        os << i << ' ';
+    return os;
+}
+
+
+vector<int> lp, sieve;
+vector<int> pr;
+vector<int> power;
+vector<int> fact(2e5 + 5, 0);
+void initpow(int x)
+{
+    power.resize(NUM);
+    power[0] = 1;
+    for (int i = 1; i < NUM; i++)
+    {
+        power[i] = (power[i - 1] * (x % mod)) % mod;
+    }
+}
+void initFactorial()
+{
+    fact.resize(NUM);
+    fact[0] = 1;
+    for (int i = 1; i < NUM; i++)
+    {
+        fact[i] = (fact[i - 1] * i) % mod;
+    }
+}
+void calc_sieve()
+{
+    sieve.resize(NUM + 1, 0);
+    for (int x = 2; x <= NUM; x++)
+    {
+        if (sieve[x])
+            continue;
+        for (int u = x; u <= NUM; u += x)
+        {
+            sieve[u] = x;
+        }
+    }
+}
+void primefactor()
+{
+    lp.resize(N + 1, 0);
+    for (int i = 2; i <= N; ++i)
+    {
+        if (lp[i] == 0)
+        {
+            lp[i] = i;
+            pr.push_back(i);
+        }
+        for (int j = 0; j < (int)pr.size() && pr[j] <= lp[i] && i * pr[j] <= N; ++j)
+            lp[i * pr[j]] = pr[j];
+    }
+}
+int binpow(int a, int b)
+{
+    int res = 1;
+    while (b > 0)
+    {
+        if (b & 1)
+            res = res * a;
+        a = a * a;
+        b >>= 1;
+    }
+    return res;
+}
+int binpow(int a, int b, int mod)
+{
+    int res = 1;
+    while (b > 0)
+    {
+        if (b & 1)
+            res = (res * a) % mod;
+        a = (a * a) % mod;
+        b >>= 1;
+    }
+    return res % mod;
+}
+int gcd(int a, int b)
+{
+    if (b == 0)
+        return a;
+    else
+        return gcd(b, a % b);
+}
+int lcm(int a, int b)
+{
+    return ((a / gcd(a, b)) * b);
+}
+int inversemod(int a, int mod)
+{
+    return binpow(a, mod - 2, mod);
+}
+int divmod(int a, int b, int c)
+{
+    return ((a % c) * inversemod(b, c)) % c;
+}
+int C(int n, int k)
+{
+    if (k > n)
+        return 0;
+    int p1 = (fact[n] * inversemod(fact[k], mod)) % mod;
+    int p2 = (1 * inversemod(fact[n - k], mod)) % mod;
+    return (p1 * p2) % mod;
+}
+
+vector<int> adj[200001];
+vector<int>dist(200001,0);
+vector<int> visb(200001,0);
+vector<int> vis(200001, 0);
+
+int ans = -1;
+
+void dfs1(int val, int d) {
+
+    dist[val] = d;
+    visb[val] = 1;
+    for (int x : adj[val]) {
+        if (!visb[x]) {
+            dfs1(x,d+1);
+        }
+    }
+}
+
+void dfs2(int val, int d) {
+    if (vis[val]) return;
+    if (d > dist[val]) {
+        ans = max(ans, 2*d);
+    }
+    vis[val] = 1;
+    for (int x : adj[val]) {
+        if (!vis[x]) {
+            dfs2(x, d+1);
+        }
+    }
+}
+
+
+signed main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    int n, x, u, v;
+
+    cin >> n >> x;
+    for (int i = 0; i < n-1; i++) {
+        cin >> u >> v;
+
+        adj[u].pb(v);
+        adj[v].pb(u);
+    }
+
+    dfs1(x,0);
+
+    dfs2(1,0);
+
+   
+    cout << ans << endl;   
+}
