b1307a2a72d2f5d665c38f9b798fb9a0c402d3cb
vayer2005 <vayer2005@gmail.com>
2025-09-25 19:28:43 -0700

    E from yest

--- 24sep/E.cpp
+++ 24sep/E.cpp
@@ -4,273 +4,49 @@
  
 using namespace std;
  
-#define pb push_back
-#define ppb pop_back
-#define mp make_pair
 #define int long long
-using vpi = vector<pair<int, int>>;
-using pi = pair<int, int>;
 using vi = vector<int>;
-using vvi = vector<vector<int>>;
-#define ff first
-#define ss second
-#define all(x) x.begin(), x.end()
-#define sz(x) (int)(x).size()
-const int mod = 1e9+7;
-const int NUM = 1e6+5; 
-const int N = 1e6+5;
-const double EPS = 1e-9;
-const int INF = 1e9 + 13;
  
 typedef __gnu_pbds::tree<int, __gnu_pbds::null_type, less<int>, __gnu_pbds::rb_tree_tag, __gnu_pbds::tree_order_statistics_node_update> ordered_set;
- 
- 
- 
-vector<int> lp, sieve;
-vector<int> pr;
-vi pow2(2e5+1, 0);
-vector<int> power;
-vector<int> fact(2e5 + 5, 0);
-vector<int> inv_fact(2e5 + 5, 0);
-
-
-int binpow(int a, int b)
-{
-    int res = 1;
-    while (b > 0)
-    {
-        if (b & 1)
-            res = res * a;
-        a = a * a;
-        b >>= 1;
-    }
-    return res;
-}
-int binpow(int a, int b, int mod)
-{
-    int res = 1;
-    while (b > 0)
-    {
-        if (b & 1)
-            res = (res * a) % mod;
-        a = (a * a) % mod;
-        b >>= 1;
-    }
-    return res % mod;
-}
-
- int inversemod(int a, int mod)
-{
-    return binpow(a, mod - 2, mod);
-}
-
-void initpow(int x)
-{
-    power.resize(NUM);
-    power[0] = 1;
-    for (int i = 1; i < NUM; i++)
-    {
-        power[i] = (power[i - 1] * (x % mod)) % mod;
-    }
-}
-void initFactorial()
-{
-    fact.resize(NUM);
-    fact[0] = 1;
-    for (int i = 1; i < NUM; i++)
-    {
-        fact[i] = (fact[i - 1] * i) % mod;
-    }
-    
-    inv_fact.resize(NUM);
-    for (int i = 0; i < NUM; i++) {
-        inv_fact[i] = inversemod(fact[i], mod);
-    }
-}
-
- 
-void calc_sieve()
-{
-    sieve.resize(NUM + 1, 0);
-    for (int x = 2; x <= NUM; x++)
-    {
-        if (sieve[x])
-            continue;
-        for (int u = x; u <= NUM; u += x)
-        {
-            sieve[u] = x;
-        }
-    }
-}
-void primefactor()
-{
-    lp.resize(N + 1, 0);
-    for (int i = 2; i <= N; ++i)
-    {
-        if (lp[i] == 0)
-        {
-            lp[i] = i;
-            pr.push_back(i);
-        }
-        for (int j = 0; j < (int)pr.size() && pr[j] <= lp[i] && i * pr[j] <= N; ++j)
-            lp[i * pr[j]] = pr[j];
-    }
-}
-
-int gcd(int a, int b)
-{
-    if (b == 0)
-        return a;
-    else
-        return gcd(b, a % b);
-}
-int lcm(int a, int b)
-{
-    return ((a / gcd(a, b)) * b);
-}
-
-int divmod(int a, int b, int c)
-{
-    return ((a % c) * inversemod(b, c)) % c;
-}
-
-// T is the type of input array elements
-// V is the type of required aggregate statistic
-//source: https://gist.github.com/kartikkukreja/2e7685e1fc8dbca0001b
-// TO use : SegmentTree<int, int> st(vector.data(), n);
-// -------------------------
-// Basic node for sum segment tree.
-// Modify this struct to implement other statistics (min/max/gcd/etc.)
-// -------------------------
-struct SegmentTreeNode {
-    // 1. Per-node state
-    long long sum = 0;                 // example: store segment sum
-
-    // 2. Build leaf from single array element
-    template<typename T>
-    void assignLeaf(const T &value) {
-        sum = value;
-    }
-
-    // 3. Merge two children
-    void merge(const SegmentTreeNode &left, const SegmentTreeNode &right) {
-        sum = left.sum + right.sum;
-    }
-
-    // 4. Return the statistic represented by this node
-    long long getValue() const {
-        return sum;
-    }
-};
-
-
-
-template<class T, class V>
-class SegmentTree {
-  SegmentTreeNode* nodes;
-  int N;
-
-public:
-  SegmentTree(T arr[], int N) {
-    this->N = N;
-    nodes = new SegmentTreeNode[getSegmentTreeSize(N)];
-    buildTree(arr, 1, 0, N-1);
-  }
-  
-  ~SegmentTree() {
-    delete[] nodes;
-  }
-  
-  V getValue(int lo, int hi) {
-    SegmentTreeNode result = getValue(1, 0, N-1, lo, hi);
-    return result.getValue();
-  }
-  
-  void update(int index, T value) {
-    update(1, 0, N-1, index, value);
-  }
-  
-  private:	
-  void buildTree(T arr[], int stIndex, int lo, int hi) {
-    if (lo == hi) {
-      nodes[stIndex].assignLeaf(arr[lo]);
-      return;
-    }
-    
-    int left = 2 * stIndex, right = left + 1, mid = (lo + hi) / 2;
-    buildTree(arr, left, lo, mid);
-    buildTree(arr, right, mid + 1, hi);
-    nodes[stIndex].merge(nodes[left], nodes[right]);
-  }
-  
-  SegmentTreeNode getValue(int stIndex, int left, int right, int lo, int hi) {
-    if (left == lo && right == hi)
-      return nodes[stIndex];
-    	
-    int mid = (left + right) / 2;
-    if (lo > mid)
-      return getValue(2*stIndex+1, mid+1, right, lo, hi);
-    if (hi <= mid)
-      return getValue(2*stIndex, left, mid, lo, hi);
-    	
-    SegmentTreeNode leftResult = getValue(2*stIndex, left, mid, lo, mid);
-    SegmentTreeNode rightResult = getValue(2*stIndex+1, mid+1, right, mid+1, hi);
-    SegmentTreeNode result;
-    result.merge(leftResult, rightResult);
-    return result;
-  }
-  
-  int getSegmentTreeSize(int N) {
-    int size = 1;
-    for (; size < N; size <<= 1);
-    return size << 1;
-  }
-  
-  void update(int stIndex, int lo, int hi, int index, T value) {
-    if (lo == hi) {
-    nodes[stIndex].assignLeaf(value);
-    return;
-    }
-    
-    int left = 2 * stIndex, right = left + 1, mid = (lo + hi) / 2;
-    if (index <= mid)
-      update(left, lo, mid, index, value);
-    else
-      update(right, mid+1, hi, index, value);
-    
-    nodes[stIndex].merge(nodes[left], nodes[right]);
-  }
-};
-
-
 
 void solve() {
+    int n;
     cin >> n;
     vi a(n);
-    for (int i = 1; i <= n; i++) {
+    for (int i = 0; i < n; i++) {
         cin >> a[i];
     }
 
-    SegmentTree<int, int> st(vector.data(), n);
+    ordered_set os1;
     int ans = 0;
+    vi big(n);
+    vi small(n);
 
-    for (int i = 1; i <= n; i++) {
-        
-        //cout << great << " " << less << endl;
-        ans += great * less;
+    for (int i = 0; i < n; i++) {
+        int ord = os1.order_of_key(-1*a[i]);
+        big[i] = ord;
+        os1.insert(-1 * a[i]);
     }
-
+    
+    ordered_set os2;
+    for (int i = n-1; i >= 0; i--) {
+        int ord = os2.order_of_key(a[i]);
+        small[i] = ord;
+        os2.insert(a[i]);
+    }
+    
+    for (int i = 0; i < n; i++) {
+        //cout << big[i] << " " << small[i] << endl;
+        ans += big[i] * small[i];
+    }
+    
     cout << ans << endl;
-
-
 }
 
-
- 
 signed main() {
     ios_base::sync_with_stdio(false);
     cin.tie(0);
-
+    
     solve();
     
     return 0;
